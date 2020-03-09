a61d36279597a8b93ec26ba08110d5bb676feb20
George Dedes <gdedes@vithoulkascompass.com>
2020-03-09 17:09:33 +0200

    build and run scripts for local and travis env
    build and run scripts for local and travis env

--- scripts/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dotnet build ../src/mservicesample.ApiGateway -c release
--- scripts/local/start.sh
+++ scripts/local/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ASPNETCORE_ENVIRONMENT=local
-cd ../src/mservicesample.ApiGateway
+cd ../../src/mservicesample.ApiGateway
 dotnet run
